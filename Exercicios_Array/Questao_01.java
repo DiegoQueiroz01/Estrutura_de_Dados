a83a5f9f0ab41c0d1382fcdf77f6d607a7cac391
DiegoQueiroz01 <diqueirozs01@gmail.com>
2025-08-29 22:17:30 -0300

    Commit inicial, implementação de exercícios de array

---
+++ Exercicios_Array/Questao_01.java
@@ -0,0 +1,14 @@
+package Exercicios_Array;
+
+public class Questao_01 {
+    int[] numeros = new int[5];
+
+    public Questao_01(){
+
+    numeros[0] = 1;
+    numeros[1] = 2;
+    numeros[2] = 3;
+    numeros[3] = 4;
+    numeros[4] = 5;
+    }
+}
