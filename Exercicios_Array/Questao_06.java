ee43b14d5f6b8df032f792497e681ed3fd896585
DiegoQueiroz01 <diqueirozs01@gmail.com>
2025-09-04 19:49:07 -0300

    Commit questão 06 terminada.

--- Exercicios_Array/Questao_06.java
+++ Exercicios_Array/Questao_06.java
@@ -1,21 +1,45 @@
+/**6. Faça um programa em Java que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno, imprima o número de alunos com média
+ maior ou igual a 7.0.*/
 package Exercicios_Array;
 
 import java.util.Scanner;
 
 public class Questao_06 {
     Scanner scan = new Scanner(System.in);
-    int[] students = new int[10];
-    float [] note = new float[4];
-    String name;
-    float mean;
-    float sum = 0;
+    float [] means = new float[4];
+    int countAgreements = 0;
 
     public Questao_06(){
-        for (int i = 0; i < note.length;  i++){
-            System.out.println("Digite seu nome: " + (i + 1));
-            System.out.println("Digite sua nota" + name + ": " + (i + 1));
-            this.note[i] = scan.nextFloat();
-            sum = sum + note[i];
+        for(int j = 0; j < means.length; j++){
+            float sum = 0;
+            System.out.println("Digite seu nome " + (j + 1) + ":");
+            String name = scan.nextLine();
+
+            for (int i = 0; i < 4; i++){
+            System.out.println("Digite a nota da unidade " + (i + 1) + ":");
+            float note = scan.nextFloat();
+            sum += note;
         }
+        scan.nextLine();
+
+        float mean = sum / 4;
+        means[j] = mean;
+
+        System.out.println("Média de " + name + ": " + mean);
+        if(mean >= 7){
+                System.out.println(name + " Aprovado :)");
+                countAgreements++;
+            } else{
+                System.out.println(name + " Reprovado :(");
+            }
+    }
+    }
+    public void imprimirArray(){
+        System.out.println("Número de alunos com média maior ou igual a 7.0: " + countAgreements);
+    }
+
+    public static void main(String[] args){
+        Questao_06 questao06 = new Questao_06();
+        questao06.imprimirArray();
     }
 }
