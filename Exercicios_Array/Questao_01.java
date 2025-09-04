026085c96e747175af3d4edab1afe065e81a8ba0
DiegoQueiroz01 <diqueirozs01@gmail.com>
2025-09-04 16:16:51 -0300

    Commit brincando com matrizes.

--- Exercicios_Array/Questao_01.java
+++ Exercicios_Array/Questao_01.java
@@ -1,21 +1,35 @@
 //1. Faça um programa em Java que leia um vetor de 5 números inteiros e mostre-os.
 package Exercicios_Array;
 
+import java.util.Scanner;
 public class Questao_01 {
-    int[] numeros = new int[5];
+    int[][] numeros = new int[2][3];
+    Scanner scan = new Scanner(System.in);
 
     public Questao_01(){
 
-    numeros[0] = 1;
+    /**numeros[0] = 1;
     numeros[1] = 2;
     numeros[2] = 3;
     numeros[3] = 4;
-    numeros[4] = 5;
+    numeros[4] = 5;*/
+
+    System.out.println("Insira 6 números inteiros: ");
+        for(int i = 0; i < numeros.length; i++){
+            for(int j = 0; j < numeros[0].length; j++){
+                numeros[i][j] = scan.nextInt();
+            }
+        }
+        scan.close();
     }
 
     public void imprimirArray(){
-        for (int i = 0; i<numeros.length; i++){
-            System.out.println(numeros[i]);
+        System.out.println("Números inseridos em formato matriz: ");
+        for (int i = 0; i < numeros.length; i++){
+            for(int j =0; j < numeros[0].length; j++){
+                System.out.print(numeros[i][j] + " ");
+            }
+        System.out.println(" ");
         }
     }
 
