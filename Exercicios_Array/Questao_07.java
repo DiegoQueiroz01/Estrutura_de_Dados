3fd743b3a34648c0afdacb552f95b63c1da232c1
DiegoQueiroz01 <diqueirozs01@gmail.com>
2025-09-10 21:49:03 -0300

    comiit; implementação de questão 07, somar, multiplicar e exibiri 5 números inteiros.

---
+++ Exercicios_Array/Questao_07.java
@@ -0,0 +1,15 @@
+//7. Faça um programa em Java que leia um vetor de 5 números inteiros, mostre a soma, a multiplicação e os números.
+
+package Exercicios_Array;
+
+import java.util.Scanner;
+
+public class Questao_07 {
+    int[] numeros = new int[5];
+    int soma, multiplicacao;
+    Scanner scan = new Scanner(System.in);
+
+    public void calcular(){
+        
+    }
+}
