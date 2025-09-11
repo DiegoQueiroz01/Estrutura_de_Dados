fa76c51cc02f627da583f2064f842c56217e6a18
DiegoQueiroz01 <diqueirozs01@gmail.com>
2025-09-10 22:09:11 -0300

    resolução de questão 07.

--- Exercicios_Array/Questao_07.java
+++ Exercicios_Array/Questao_07.java
@@ -6,10 +6,34 @@ import java.util.Scanner;
 
 public class Questao_07 {
     int[] numeros = new int[5];
-    int soma, multiplicacao;
+    int numero, soma = 0;
+    int multiplicacao = 1;
     Scanner scan = new Scanner(System.in);
 
     public void calcular(){
-        
+        for(int i = 0; i <numeros.length; i++){
+            System.out.println("Insira o número " + (i+1) + ": ");
+            int numero = scan.nextInt();
+            numeros[i] = numero;
+
+            soma += numero;
+            multiplicacao *= numero;
+        }
+
+        System.out.println("Números usados para o calculo: ");
+        for(int i = 0; i < numeros.length; i++){
+            System.out.println(numeros[i] + " ");
+        }
+    }
+
+    public void imprimirCalculo(){
+        System.out.println("A soma dos números é: " + soma);
+        System.out.println("A múltiplicação dos números é: " + multiplicacao);
+    }
+
+    public static void main(String[] args){
+        Questao_07 questao07 = new Questao_07();
+        questao07.calcular();
+        questao07.imprimirCalculo();
     }
 }
